f704649e979a1c1525664527160810034bdba569
ollbap <ollbap@gmail.com>
2018-01-11 20:11:50 +0100

    Added spyder project config

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ DEBUG_MODE = False
 
 AUTO_CHECK_TIMER_THREAD = None
 LAST_CHECK_RESULT = None
-LAST_CHECK_STATUS = None 
+LAST_CHECK_STATUS = None
 
 AUTO_CHECK_MODE = CONFIG.getboolean('InitialState', 'auto_check')
 AUTO_CHECK_FREQUENCY_SECONDS = 60 * CONFIG.getint('InitialState', 'auto_check_frequency_minutes')
@@ -76,32 +76,32 @@ def check_now():
         global GIT_OFFLINE_ROOT_PATHS
         global LAST_CHECK_RESULT
         global LAST_CHECK_STATUS
-        
+
         updateIconAsWorking()
         myPrint("Checking now")
-        
+
         m1 = gitCheckDirtyStateRecursive(GIT_ONLINE_ROOT_PATHS, ONLINE_CHECK_MODE)
         m2 = gitCheckDirtyStateRecursive(GIT_OFFLINE_ROOT_PATHS, ONLINE_CHECK_MODE)
         states = dict(m1.items() + m2.items())
-    
+
         maxState = DirtyState.CLEAN
         for path, state in states.items():
             if state != DirtyState.CLEAN:
                 myPrint("%15s | %s" % (state.name, path))
             if state.value > maxState.value:
                 maxState = state
-    
+
         updateIconState(maxState)
         LAST_CHECK_RESULT = states
         LAST_CHECK_STATUS = maxState
     except:
         traceback.print_exc()
         myGuiMessage("Check failed", traceback.format_exc())
-    
+
 def showLastDirtyDirectories():
     showDirtyDirectories(LAST_CHECK_RESULT)
     check_now()
-    
+
 def showLastDirtyDirectories_FromGuiBackground(ignored):
     global LAST_CHECK_RESULT
     t = threading.Thread(target=showLastDirtyDirectories)
@@ -133,19 +133,19 @@ def change_online_mode (item):
 def quit_callback(ignored):
     global AUTO_CHECK_TIMER_THREAD
     global GTK_ICON
-    
+
     GTK_ICON.set_visible(False)
     myPrint("Quitting")
     gtk.main_quit()
 
 def make_menu(event_button, event_time, data=None):
     menu = gtk.Menu()
-    
+
     show_dirty_item = gtk.MenuItem("Show Dirty")
     menu.append(show_dirty_item)
     show_dirty_item.connect_object("activate", showLastDirtyDirectories_FromGuiBackground, ())
     show_dirty_item.show()
-    
+
     check_item = gtk.MenuItem("Check")
     menu.append(check_item)
     check_item.connect_object("activate", check_from_gui, ())
@@ -162,12 +162,12 @@ def make_menu(event_button, event_time, data=None):
     menu.append(online_item)
     online_item.connect_object("activate", change_online_mode, (online_item))
     online_item.show()
-    
+
     kill_item = gtk.MenuItem("Quit")
     menu.append(kill_item)
     kill_item.connect_object("activate", quit_callback, ())
     kill_item.show()
-    
+
     menu.popup(None, None, None, event_button, event_time)
 
 def on_right_click(data, event_button, event_time):
@@ -179,7 +179,7 @@ def on_left_click(event):
         check_from_gui(())
     else:
         showLastDirtyDirectories_FromGuiBackground(())
-    
+
 def autoCheckTimer():
     global AUTO_CHECK_FREQUENCY_SECONDS
     global AUTO_CHECK_MODE
@@ -193,7 +193,7 @@ def autoCheckTimer():
                 dirtyTimesToForce += 1
             else:
                 dirtyTimesToForce = 0
-                
+
             if dirtyTimesToForce >= FORCE_AUTO_SHOW_CHECK_DIALOG_TIMES:
                 showLastDirtyDirectories_FromGuiBackground(())
                 dirtyTimesToForce = 0
@@ -215,7 +215,7 @@ def initStatusIcon():
     #So application does not hang
     AUTO_CHECK_TIMER_THREAD.daemon = True
     AUTO_CHECK_TIMER_THREAD.start()
-    
+
 if __name__ == '__main__':
     initStatusIcon()
     gtk.main()
