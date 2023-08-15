6a8cb36e86749cfe07099c9eb227ab09567af99e
Pablo <ollbap@gmail.com>
2023-08-15 22:26:36 +0200

    Instructions to setup a virtual env

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#To install virtual environment: 
+python -m venv venv
+
+#Install requirements
+venv/bin/pip install gitpython pystray Pillow==9.5.0
+
+#For it to work well I used to install this in the system
+apt install python-tk
